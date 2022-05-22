b45eda13ccc108c83d2ab19984cd5ab05ad772aa
Badger <13.zrka@gmail.com>
2022-05-22 16:07:18 +0300

    fan

--- app/src/main/java/com/badger/familyorgfe/features/appjourney/adding/AddingScreen.kt
+++ app/src/main/java/com/badger/familyorgfe/features/appjourney/adding/AddingScreen.kt
@@ -5,16 +5,21 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.badger.familyorgfe.R
@@ -23,6 +28,7 @@ import com.badger.familyorgfe.ui.elements.BaseDialog
 import com.badger.familyorgfe.ui.elements.BaseToolbar
 import com.badger.familyorgfe.ui.theme.FamilyOrganizerTheme
 import com.badger.familyorgfe.utils.BackHandler
+import kotlin.math.roundToInt
 
 @Composable
 fun AddingScreen(
@@ -30,83 +36,65 @@ fun AddingScreen(
     navOnBack: () -> Unit,
     viewModel: IAddingViewModel = hiltViewModel<AddingViewModel>()
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-    ) {
+    val onBack: () -> Unit = {
+        val event = IAddingViewModel.Event.OnBackClicked
+        viewModel.onEvent(event)
+        navOnBack()
+    }
+    BackHandler(onBack = onBack)
 
-        val onBack: () -> Unit = {
-            val event = IAddingViewModel.Event.OnBackClicked
-            viewModel.onEvent(event)
-            navOnBack()
+    val fabHeight = 72.dp //FabSize+Padding
+    val fabHeightPx = with(LocalDensity.current) { fabHeight.roundToPx().toFloat() }
+    val fabOffsetHeightPx = remember { mutableStateOf(0f) }
+
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+
+                val delta = available.y
+                val newOffset = fabOffsetHeightPx.value + delta
+                fabOffsetHeightPx.value = newOffset.coerceIn(-fabHeightPx, 0f)
+
+                return Offset.Zero
+            }
         }
-        BackHandler(onBack = onBack)
+    }
 
-        Toolbar(
-            onBackClicked = onBack,
-            onDoneClicked = {}
-        )
-
-        val fridgeItems by viewModel.items.collectAsState()
-        val expandedItemId by viewModel.expandedItemId.collectAsState()
-        val deleteDialog by viewModel.deleteItemDialog.collectAsState()
-
-        LazyColumn(
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
             modifier = modifier
-                .fillMaxWidth()
-                .weight(1f),
+                .fillMaxSize()
         ) {
-            item {
-                Spacer(modifier = Modifier.height(16.dp))
-            }
-            items(
-                items = fridgeItems,
-                key = { item -> item.id }
-            ) { item ->
-                FridgeListItem(
-                    item = item,
-                    isExpanded = item.id == expandedItemId,
-                    onExpand = {
-                        val event = IAddingViewModel.Event.OnItemExpanded(item.id)
-                        viewModel.onEvent(event)
-                    },
-                    onCollapse = {
-                        val event = IAddingViewModel.Event.OnItemCollapsed
-                        viewModel.onEvent(event)
-                    },
-                    onEdit = {
-
-                    },
-                    onDelete = { deletedItem ->
-                        val event = IAddingViewModel.Event.RequestDeleteItemDialog(deletedItem)
-                        viewModel.onEvent(event)
-                    }
-                )
-                Spacer(
-                    modifier = Modifier.height(8.dp)
-                )
-            }
+            Toolbar(
+                onBackClicked = onBack,
+                onDoneClicked = {}
+            )
+            Listing(
+                modifier = modifier,
+                viewModel = viewModel,
+                nestedScrollConnection = nestedScrollConnection
+            )
         }
 
-        deleteDialog?.let { fridgeItem ->
-
-            BaseDialog(
-                titleText = stringResource(id = R.string.fridge_toolbar_title),
-                descriptionText = stringResource(
-                    id = R.string.fridge_delete_product_description,
-                    fridgeItem.name
-                ),
-                dismissText = stringResource(id = R.string.fridge_delete_product_dismiss),
-                actionText = stringResource(id = R.string.fridge_delete_product_action),
-                onDismissClicked = {
-                    val event = IAddingViewModel.Event.DismissDeleteDialog
-                    viewModel.onEvent(event)
-                },
-                onActionClicked = {
-                    val event = IAddingViewModel.Event.DeleteItem(fridgeItem)
-                    viewModel.onEvent(event)
-                })
-
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+                .offset { IntOffset(x = 0, y = -fabOffsetHeightPx.value.roundToInt()) },
+            backgroundColor = FamilyOrganizerTheme.colors.primary,
+            onClick = {
+                val event = IAddingViewModel.Event.OnAddClicked
+                viewModel.onEvent(event)
+            }
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(28.dp)
+                    .align(Alignment.Center),
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = null,
+                tint = FamilyOrganizerTheme.colors.whitePrimary
+            )
         }
     }
 }
@@ -146,7 +134,7 @@ private fun Toolbar(
                 .clickable(
                     indication = null,
                     interactionSource = remember { MutableInteractionSource() }
-                ) { onDoneClicked },
+                ) { onDoneClicked() },
             painter = painterResource(id = R.drawable.ic_done),
             contentDescription = null,
             tint = FamilyOrganizerTheme.colors.blackPrimary
@@ -154,5 +142,74 @@ private fun Toolbar(
 
 
         Spacer(modifier = Modifier.width(16.dp))
+    }
+}
+
+@Composable
+private fun ColumnScope.Listing(
+    modifier: Modifier,
+    viewModel: IAddingViewModel,
+    nestedScrollConnection: NestedScrollConnection
+) {
+    val fridgeItems by viewModel.items.collectAsState()
+    val expandedItemId by viewModel.expandedItemId.collectAsState()
+    val deleteDialog by viewModel.deleteItemDialog.collectAsState()
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxWidth()
+            .weight(1f).nestedScroll(nestedScrollConnection),
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        items(
+            items = fridgeItems,
+            key = { item -> item.id }
+        ) { item ->
+            FridgeListItem(
+                item = item,
+                isExpanded = item.id == expandedItemId,
+                onExpand = {
+                    val event = IAddingViewModel.Event.OnItemExpanded(item.id)
+                    viewModel.onEvent(event)
+                },
+                onCollapse = {
+                    val event = IAddingViewModel.Event.OnItemCollapsed
+                    viewModel.onEvent(event)
+                },
+                onEdit = {
+
+                },
+                onDelete = { deletedItem ->
+                    val event = IAddingViewModel.Event.RequestDeleteItemDialog(deletedItem)
+                    viewModel.onEvent(event)
+                }
+            )
+            Spacer(
+                modifier = Modifier.height(8.dp)
+            )
+        }
+    }
+
+    deleteDialog?.let { fridgeItem ->
+
+        BaseDialog(
+            titleText = stringResource(id = R.string.fridge_toolbar_title),
+            descriptionText = stringResource(
+                id = R.string.fridge_delete_product_description,
+                fridgeItem.name
+            ),
+            dismissText = stringResource(id = R.string.fridge_delete_product_dismiss),
+            actionText = stringResource(id = R.string.fridge_delete_product_action),
+            onDismissClicked = {
+                val event = IAddingViewModel.Event.DismissDeleteDialog
+                viewModel.onEvent(event)
+            },
+            onActionClicked = {
+                val event = IAddingViewModel.Event.DeleteItem(fridgeItem)
+                viewModel.onEvent(event)
+            })
+
     }
 }
