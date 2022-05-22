30cc928700a110942d62120f9840e925fec354b9
Badger <13.zrka@gmail.com>
2022-05-23 01:55:16 +0300

    Ready fe adding

--- app/src/main/java/com/badger/familyorgfe/ext/BusinessExt.kt
+++ app/src/main/java/com/badger/familyorgfe/ext/BusinessExt.kt
@@ -2,8 +2,20 @@ package com.badger.familyorgfe.ext
 
 import com.badger.familyorgfe.data.model.Product
 import com.badger.familyorgfe.features.appjourney.fridge.fridgeitem.FridgeItem
+import org.threeten.bp.Duration
+import org.threeten.bp.LocalDateTime
 
 fun Product.toFridgeItem(): FridgeItem {
+    val expDaysLeft = expiryDate?.let { expDate ->
+        if (LocalDateTime.now() == expDate) {
+            0
+        } else {
+            Duration.between(
+                LocalDateTime.now(),
+                expDate
+            ).toDays() + 1
+        }
+    }?.takeIf { it > 0 } ?: 0
 
     return FridgeItem(
         id = id,
@@ -11,7 +23,7 @@ fun Product.toFridgeItem(): FridgeItem {
         quantity = quantity,
         measure = measure,
         category = category,
-        expDaysLeft = 5,
+        expDaysLeft = expDaysLeft.toInt(),
         expStatus = Product.ExpirationStatus.BAD_SOON
     )
 }
