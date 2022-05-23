66ac9fceaba4a5414c2a19bf03cce7d2a47bfa67
Badger <13.zrka@gmail.com>
2022-05-24 00:50:37 +0300

    fix expiration days visibility in fridge item

--- app/src/main/java/com/badger/familyorgfe/features/appjourney/fridge/domain/GetAllFridgeItemsUseCase.kt
+++ app/src/main/java/com/badger/familyorgfe/features/appjourney/fridge/domain/GetAllFridgeItemsUseCase.kt
@@ -5,6 +5,7 @@ import com.badger.familyorgfe.data.model.Product
 import com.badger.familyorgfe.data.repository.IDataStoreRepository
 import com.badger.familyorgfe.data.source.products.ProductsApi
 import com.badger.familyorgfe.data.source.products.json.GetProductsJson
+import com.badger.familyorgfe.ext.toFridgeItem
 import com.badger.familyorgfe.features.appjourney.fridge.fridgeitem.FridgeItem
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
@@ -30,14 +31,4 @@ class GetAllFridgeItemsUseCase @Inject constructor(
     }
 
     private fun List<Product>.toFridgeItems() = map { item -> item.toFridgeItem() }
-
-    private fun Product.toFridgeItem() = FridgeItem(
-        id = id,
-        name = name,
-        quantity = quantity,
-        measure = measure,
-        category = category,
-        expDaysLeft = 5,
-        expStatus = Product.ExpirationStatus.BAD_SOON
-    )
 }
